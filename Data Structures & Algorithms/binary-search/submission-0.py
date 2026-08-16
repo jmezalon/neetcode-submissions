d6d2d394986e4c1501938eceeace33d891640ccd
Max Mezalon <43793449+jmezalon@users.noreply.github.com>
2026-08-16 10:28:55 -0400

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        start, last = 0, len(nums) - 1
+
+        while start <= last:
+            mid = (start + last) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            elif nums[mid] > target:
+                last = mid - 1
+
+        return -1
